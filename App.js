71cb53f52bd1c9c196ed0e25f9e6b2ff716d42f4
PSPHARICHARAN143 <21fh1a05a9@drkvsrit.in>
2025-06-08 19:31:03 +0530

    Src

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FeedbackList from "./components/FeedbackList";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Feedback Form</h2>
+      {/* your form component here (if any) */}
+
+      <FeedbackList />
+    </div>
+  );
+}
+
+export default App;
